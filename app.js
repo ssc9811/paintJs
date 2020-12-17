0ff520687e14f40b6f24ce50787a75a96147dfab
ssc9811 <poer2418@naver.com>
2020-12-17 14:38:25 +0900

    Changing Color

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
 
 canvas.width = 700;
 canvas.height = 700;
 
 ctx.strokeStyle = "#2c2c2c";
-ctx.lineWidth = 50;
+ctx.lineWidth = 2.5;
 
 let paingting = false;
 
@@ -29,10 +30,11 @@ function onMouseMove(event){
     }
 }
 
-function onMouseDown(event){
-    paingting = true;
-}
 
+function handleColorClick(event){
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color
+}
 
 if(canvas){
     canvas.addEventListener("mousemove",onMouseMove);
@@ -40,3 +42,7 @@ if(canvas){
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting)
 }
+
+
+Array.from(colors).forEach(color => 
+    color.addEventListener("click" , handleColorClick));
