c03425bc5f9f7e479c5c8def3b04149c4b1a27d7
ssc9811 <poer2418@naver.com>
2020-12-17 14:29:17 +0900

    painting line

--- app.js
+++ app.js
@@ -1,26 +1,42 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 50;
 
 let paingting = false;
 
 function stopPainting(){
-    paingting = false
+    paingting = false;
+}
+
+function startPainting(){
+    paingting = true;
 }
 
 function onMouseMove(event){
     const x = event.offsetX;
     const y = event.offsetY;
+    if(!paingting){
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    }else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 }
+
 function onMouseDown(event){
     paingting = true;
-}
-function onMouseUp(event){
-    stopPainting();
 }
 
 
 if(canvas){
     canvas.addEventListener("mousemove",onMouseMove);
-    canvas.addEventListener("mousedown",onMouseDown);
-    canvas.addEventListener("mouseup",onMouseUp);
+    canvas.addEventListener("mousedown",startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting)
 }
